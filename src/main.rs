22385fe715a21d8daff6ae82016700e6c4ae796e
michael glaviano <michael.glaviano@gmail.com>
2025-11-15 11:56:59 -0600

    Add log and env_logger dependencies with initialization

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ mod ui;
 use state::App;
 
 fn main() -> iced::Result {
+    env_logger::Builder::from_default_env()
+        .format_timestamp_millis()
+        .init();
+
     iced::application("Cineplex", App::update, App::view)
         .subscription(App::subscription)
         .run()
